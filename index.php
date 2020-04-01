a9a41c99f05f0e667e10fac5c20811dea8a0ea43
kanayanindya <wintorogaulz@gmail.com>
2020-04-01 15:47:51 +0700

    menambah paragraf pada index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <title>Coba GIt Remote kedua</title>
 </head>
 <body>
-    <h1>Jello world</h1>
-    
+    <h1>Jello world </h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio quae eveniet illo quis omnis facilis! Quos tempora tempore, eligendi, hic nostrum voluptas velit magni ipsam officia ducimus culpa minus ex.</p>
 </body>
 </html>
